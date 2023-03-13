ad685e2fe5909e9398f1165c58fd647824d9349a
Joey <9813078+PXshadow@users.noreply.github.com>
2023-03-13 04:08:39 +0100

    add .gitignore

--- bin/vendor/Main_Main/Main_Main.go
+++
@@ -1,8 +0,0 @@
-package Main_Main
-        type Main_Main struct {
-            
-        }
-        
-        func main()  {
-		unknown("Hello World", map[string]interface{}{ "fileName" : "Main.hx", "lineNumber" : 5, "className" : "Main", "methodName" : "main" })
-	}
